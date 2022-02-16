7a07a5fdbdc793237fc9d1bb3983e983e1c696c8
m-jirka <mrtnjrk@gmail.com>
2022-02-16 11:54:17 +0100

    empty

---
+++ _build/jupyter_execute/intro.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Numerické metody - cvičení
+# ## Letní semestr 2021/2022, KFE FJFI ČVUT v Praze
+# ### Rozvrh
+# * Čtvrtek 7:30 - 9:10
+# * Trojanova 13, učebna T-124
+# * Martin JIRKA, jirkama1@fjfi.cvut.cz
+# 
+# ### Podmínky pro udělění zápočtu
+# * maximálně 3 absence
+# * zisk alespoň 50 % bodů za úkoly zadávané na cvičeních. Tyto úkoly vypracované v Jupyter Notebook odevzdávejte na výše uvedený e-mail do začátku následujícího cvičení.
+# 
+# ### Používané nástroje
+# Na cvičeních budeme používat Python v on-line prostředí [Jupyter notebook](https://jupyter.org/). Jupyter notebook lze vytvářet a editovat on-line např. v [Google Colab](https://colab.research.google.com/). Pro vypracování úkolů a off-line práci s Jupyter notebooky na počítači je nejjednodušší nainstalovat si programový balík [Anaconda](https://www.anaconda.com/) (dostupné pro Windows/Mac/Linux), který obsahuje všechny potřebné knihovny (a navíc Python editor [spyder](https://www.spyder-ide.org/) a mnoho dalšího):
+# 1. [Návod na instalaci](https://docs.anaconda.com/anaconda/install/)
+# 2. [Návod na spuštení Jupyter notebook](https://docs.anaconda.com/anaconda/user-guide/getting-started/)
+# 
+# Materiály ke cvičením vycházejí z těchto [podkladů](http://kfe.fjfi.cvut.cz/~vachal/edu/nme/cviceni/index.html).
