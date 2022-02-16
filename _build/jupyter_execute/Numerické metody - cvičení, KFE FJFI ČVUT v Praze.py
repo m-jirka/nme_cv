e1b1f281db195769b091affb5680b32eefd51c35
m-jirka <mrtnjrk@gmail.com>
2022-02-16 08:30:31 +0100

    uvod

---
+++ _build/jupyter_execute/Numerické metody - cvičení, KFE FJFI ČVUT v Praze.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Numerické metody - cvičení
+# ## Letní semestr 2021/2022, KFE FJFI ČVUT v Praze
+# ***
+#  * [Infromace pro cvičení LS 2021/2022](https://m-jirka.github.io/nme_cv/)
+# 
+# ***
+# Martin JIRKA, jirkama1@fjfi.cvut.cz
+# 
--- _build/jupyter_execute/intro.py
+++ _build/jupyter_execute/intro.py
@@ -10,17 +10,11 @@
 # 
 # ### Podmínky pro udělění zápočtu
 # * maximálně 3 absence
-# * vypracování zápočtové úlohy a její obhájení nebo průbežně plnění úkolů a jejich odevzdání do začátku následujícího cvičení (používejte Python nebo Matlab)
+# * zisk alespoň 50 % bodů za úkoly zadávané na cvičeních. Tyto úkoly vypracované v Jupyter Notebook odevzdávejte na výše uvedený e-mail do začátku následujícího cvičení.
 # 
 # ### Používané nástroje
-# Na cvičeních budeme používat Python v on-line prostředí [Jupyter notebook](https://jupyter.org/). Pro vypracování úkolů je třeba si Python prostředí nainstalovat. Pro off-line práci s Jupyter notebook na počítači je nejjednodušší používat programový balík [Anaconda](https://www.anaconda.com/) (dostupné pro Windows/Mac/Linux), který obsahuje všechny potřebné knihovny (a navíc Python editor [spyder](https://www.spyder-ide.org/) a mnoho dalšího).
+# Na cvičeních budeme používat Python v on-line prostředí [Jupyter notebook](https://jupyter.org/). Jupyter notebook lze vytvářet a editovat on-line např. v [Google Colab](https://colab.research.google.com/). Pro vypracování úkolů a off-line práci s Jupyter notebooky na počítači je nejjednodušší nainstalovat si programový balík [Anaconda](https://www.anaconda.com/) (dostupné pro Windows/Mac/Linux), který obsahuje všechny potřebné knihovny (a navíc Python editor [spyder](https://www.spyder-ide.org/) a mnoho dalšího):
 # 1. [Návod na instalaci](https://docs.anaconda.com/anaconda/install/)
 # 2. [Návod na spuštení Jupyter notebook](https://docs.anaconda.com/anaconda/user-guide/getting-started/)
 # 
 # Materiály ke cvičením vycházejí z těchto [podkladů](http://kfe.fjfi.cvut.cz/~vachal/edu/nme/cviceni/index.html).
-
-# In[ ]:
-
-
-
-
